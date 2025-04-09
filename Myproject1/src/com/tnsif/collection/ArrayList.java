092b3bf14d35ed61b0b71ff3050002aa36812cbc
Sindhu S <Sindhu S@LAPTOP-3BERK028>
2025-04-09 15:07:34 +0530

    initial commit

---
+++ Myproject1/src/com/tnsif/collection/ArrayList.java
@@ -0,0 +1,12 @@
+package com.tnsif.collection;
+
+public class ArrayList {
+	public static void main(String[] args) {
+		
+	}
+
+	public ArrayList() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
